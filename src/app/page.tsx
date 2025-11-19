152ed774e2449fcac3abf463049a715b6bb108af
Patrick MacDonald <patrick@patmac.ca>
2025-11-18 19:16:37 -0700

    removed suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { RecentForks } from '@/components/RecentForks'
 import { getForkData } from '@/lib/DAL/fetchDaysSince'
 import { getRecentForks } from '@/lib/DAL/fetchRecentForks'
 
-export default async  function Home(props: PageProps<'/'>) {
+export default async function Home(props: PageProps<'/'>) {
 	const forkDataPromise = getForkData()
 	const recentForksPromise = props.searchParams.then((search) =>
 		getRecentForks({
@@ -20,9 +20,7 @@ export default async  function Home(props: PageProps<'/'>) {
 		<main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden p-4 text-center">
 			<div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-zinc-800/20 via-zinc-950 to-zinc-950" />
 
-			<Suspense>
-				<DaysSinceComponent forkDataPromise={forkDataPromise} />
-			</Suspense>
+			<DaysSinceComponent forkDataPromise={forkDataPromise} />
 
 			<div className="fade-in mt-16 animate-in delay-500 duration-1000">
 				<Suspense>
